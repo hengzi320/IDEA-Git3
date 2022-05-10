2d5facb3967a6508884f111cae356449b3faa272
hengzi <hengzi@421.com>
2022-05-10 11:15:29 +0800

    Git1 commit

---
+++ git1/src/main/java/com/lxit/servic/gonju.java
@@ -0,0 +1,23 @@
+package com.lxit.servic;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.apache.commons.dbutils.QueryRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class gonju {
+    public static void main(String args[]) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        Properties pro= new Properties();
+        pro.load(gonju.class.getClassLoader().getResourceAsStream("druid.properties"));
+        DataSource ds= DruidDataSourceFactory.createDataSource(pro);
+
+        QueryRunner qr=new QueryRunner(ds);
+        String sql="UPDATE student2 set age=?";
+        qr.update(sql,19);
+        Connection connection =ds.getConnection();
+        System.out.println(connection);
+    }
+}
